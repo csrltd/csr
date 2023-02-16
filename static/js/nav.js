b8d7e62e0e0f4b7a843532d120822c074b02b64c
Olivier <olivier@Oliviers-MacBook-Pro.local>
2023-02-16 14:24:28 +0200

    corrections made closeBtn dissapear when a link is clicked in nav js

--- static/js/nav.js
+++ static/js/nav.js
@@ -92,9 +92,12 @@ window.addEventListener('scroll', () => {
 
 
 //Closing menu when link clicked
+// corrections made closeBtn dissapear when a link is clicked
 
 navLinks.forEach((link) => {
   link.addEventListener('click', () => {
+    closeBtn.style.display = 'none'
+    navToggle.style.display = 'block'
     if (window.innerWidth < 1200) {
       mobileMenu.style.display = 'none'
     }
