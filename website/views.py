ee24dde52bbbfb686637f5c6629de90b93f22956
alphonse SIBOMANA <alphonsesibomana@alphonses-MacBook-Pro.local>
2023-02-17 16:24:12 +0200

    launching updates

--- website/views.py
+++ website/views.py
@@ -15,7 +15,7 @@ def index(request):
             form.save()
             return redirect('thank-you')
     context = {'form': form, 'page_title':page_title}
-    return render(request, 'coming-soon.html', context)
+    return render(request, 'index.html', context)
 
 def blog(request, slug):
     page_title = 'Blog posts'
